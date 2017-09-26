bba9e507a1bc8729daf7e813e420c06cbd6f9ed0
Thibault Souef <tsouef@letudiant.fr>
2017-09-26 19:27:17 +0200

    WIP Server

--- server/models/user.js
+++ server/models/user.js
@@ -10,20 +10,24 @@ const userSchema = new Schema({
 });
 
 // On Save Hook, enrypt password
+// // Before saving a model, run this function
 userSchema.pre('save', function(next) {
   const user = this;
 
+  // generate a salt then run callback
   bcrypt.genSalt(10, function(err, salt) {
     if (err) { return next(err); }
 
-    bcrypt.hash(user.password, sale, null, function(err, hash) {
+    // hash our password using the salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
       if (err) { return next(err); }
 
+      // overwrite plain text password with encrypted password
       user.password = hash;
-      next()
-    })
-  })
-})
+      next();
+    });
+  });
+});
 
 // Create the model class
 const ModelClass = mongoose.model('user', userSchema);
