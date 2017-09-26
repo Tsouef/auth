79b04ab29db3d8e0c62e6fe121c5273d7134fdcd
Thibault Souef <souef.thibault@gmail.com>
2017-09-26 09:04:25 +0200

    add model user and db

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
+const mongoose = require('mongoose');
+
+// DB Setup
+mongoose.connect('mongodb://localhost:auth/auth');
 
 // App Setup
 app.use(morgan('combined'));
@@ -11,7 +15,7 @@ app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
 // Server Setup
-const port = process.env.PORt || 3090;
+const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server listening on:', port);
